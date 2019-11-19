ff3441755396bddd706d76a44b19c82f815d56f5
rpiaggio <rpiaggio@gmail.com>
2019-11-19 19:06:59 -0300

    Simplification of demo component.

--- client/src/main/scala/spatutorial/client/components/Motd2.scala
+++ client/src/main/scala/spatutorial/client/components/Motd2.scala
@@ -9,44 +9,33 @@ import spatutorial.client.services.Crystal.{MotdAlgebra, Slice}
 import diode.react.ReactPot._
 
 /**
-  * This is a simple component demonstrating how to display async data coming from the server
-  */
+ * This is a simple component demonstrating how to display async data coming from the server
+ */
 object Motd2 {
-
-//  implicit val ioTimer = cats.effect.IO.timer
-//  implicit val ioCS: ContextShift[IO] = IO.contextShift(global)
-//
-//  val MotdFlow = Flow.flow(Global.InterpreterIO.Motd.motdStream)
-
-  // create the React component for holding the Message of the Day
   val Motd = ScalaComponent.builder[Slice[IO, MotdAlgebra, Pot[String]]]("Motd")
     .render_P { p =>
       Panel(Panel.Props("Message of the day"),
-        // render messages depending on the state of the Pot
 
-        p.flow( motd =>
-<.div(
-//          motd.fold(<.p("Loading..."))(msg => <.p(msg))
-//
-          motd.get.renderPending(_ > 500, _ => <.p("Loading...")),
-          motd.get.renderFailed(ex => <.p("Failed to load")),
-          motd.get.render(m => <.p(m))
-)
-        ),
-
+        p.flow { motdOpt =>
+          val motd = Pot.fromOption(motdOpt).flatten
+          <.div(
+            motd.renderPending(_ > 500, _ => <.p("Loading...")),
+            motd.renderFailed(ex => <.p("Failed to load")),
+            motd.render(m => <.p(m))
+          )
+        },
 
         Button(Button.Props(
-
-          Callback{
+          Callback {
             p.actions.updateMotd.unsafeRunSync()
           }
           , CommonStyle.danger), Icon.refresh, " Update")
       )
     }
-//    .componentDidMount(scope =>
-//       update only if Motd is empty
-//      Callback.when(scope.props.value.isEmpty)(scope.props.dispatchCB(UpdateMotd()))
-//    )
+    //    .componentDidMount(scope =>
+    //       update only if Motd is empty
+    //      Callback.when(scope.props.value.isEmpty)(scope.props.dispatchCB(UpdateMotd()))
+    //    )
     .build
 
   def apply(props: Slice[IO, MotdAlgebra, Pot[String]]) = Motd(props)
