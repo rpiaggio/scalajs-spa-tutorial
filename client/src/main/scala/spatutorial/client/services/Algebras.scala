ec305408160ad51fb70afed49d0b10f0626758a1
rpiaggio <rpiaggio@gmail.com>
2019-11-28 17:54:55 -0300

    Todos algebra.

--- client/src/main/scala/spatutorial/client/services/Algebras.scala
+++ client/src/main/scala/spatutorial/client/services/Algebras.scala
@@ -6,13 +6,11 @@ import crystal._
 import autowire._
 import boopickle.Default._
 import diode.data._
-import spatutorial.client.services.AppState.motdView
-import spatutorial.shared.Api
+import spatutorial.shared.{Api, TodoItem}
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.global
 import scala.util.{Failure, Success}
-
 import scala.language.higherKinds
 
 object Algebras {
@@ -46,5 +44,71 @@ object Algebras {
       } yield ()
   }
 
-  implicit object MotdAlgebraIO extends MotdAlgebraInterpreter[IO](motdView)
+  implicit object MotdAlgebraIO extends MotdAlgebraInterpreter[IO](AppState.motdView)
+
+  trait TodoAlgebra[F[_]] {
+    def refreshTodos(): F[Unit]
+    def updateAllTodos(todos: Seq[TodoItem]): F[Unit]
+    def updateTodo(item: TodoItem): F[Unit]
+    def deleteTodo(item: TodoItem): F[Unit]
+  }
+
+  class TodoAlgebraInterpreter[F[_] : Effect](lens: SignallingLens[F, Pot[Todos]]) extends TodoAlgebra[F] {
+    implicit private val ec: ExecutionContext = global
+
+    protected object Ajax {
+      def getAllTodos(): F[Seq[TodoItem]] =
+        Async[F].async { cb =>
+          AjaxClient[Api].getAllTodos().call().onComplete {
+            case Success(value) => cb(Right(value))
+            case Failure(t) => cb(Left(t))
+          }
+        }
+
+      def updateTodo(item: TodoItem): F[Seq[TodoItem]] =
+        Async[F].async { cb =>
+          AjaxClient[Api].updateTodo(item).call().onComplete {
+            case Success(value) => cb(Right(value))
+            case Failure(t) => cb(Left(t))
+          }
+        }
+
+      def deleteTodo(itemId: String): F[Seq[TodoItem]] =
+        Async[F].async { cb =>
+          AjaxClient[Api].deleteTodo(itemId).call().onComplete {
+            case Success(value) => cb(Right(value))
+            case Failure(t) => cb(Left(t))
+          }
+        }
+    }
+
+    def refreshTodos(): F[Unit] =
+      for {
+        todos <- Ajax.getAllTodos()
+        _ <- updateAllTodos(todos)
+      } yield ()
+
+    def updateAllTodos(todos: Seq[TodoItem]): F[Unit] =
+      lens.set(Ready(Todos(todos)))
+
+    def updateTodo(item: TodoItem): F[Unit] =
+      for {
+        todosPot <- lens.get
+        updatedTodos = todosPot.map(_.updated(item))
+        _ <- lens.set(updatedTodos)
+        newTodos <- Ajax.updateTodo(item)
+        _ <- updateAllTodos(newTodos)
+      } yield ()
+
+    def deleteTodo(item: TodoItem): F[Unit] =
+      for {
+        todosPot <- lens.get
+        updatedTodos = todosPot.map(_.remove(item))
+        _ <- lens.set(updatedTodos)
+        newTodos <- Ajax.deleteTodo(item.id)
+        _ <- updateAllTodos(newTodos)
+      } yield ()
+  }
+
+  implicit object TodoAlgebraIO extends TodoAlgebraInterpreter[IO](AppState.todosView)
 }
--- client/src/main/scala/spatutorial/client/services/AppState.scala
+++ client/src/main/scala/spatutorial/client/services/AppState.scala
@@ -8,16 +8,16 @@ import crystal._
 
 import monocle.macros.Lenses
 
-import scala.language.higherKinds
-
 object AppState {
-  implicit private val timerIO = cats.effect.IO.timer(global)
+  implicit private val timerIO: Timer[IO] = cats.effect.IO.timer(global)
   implicit private val csIO: ContextShift[IO] = IO.contextShift(global)
 
   @Lenses
   case class RootModel(todos: Pot[Todos], motd: Pot[String])
 
   val rootModel = Model[IO, RootModel](RootModel(Empty, Empty))
+
+  val todosView = rootModel.view(RootModel.todos)
 
   val motdView = rootModel.view(RootModel.motd)
 }
