2ff6b8d6d6811cc0fb1f11aaab0e84545a6e46f5
rpiaggio <rpiaggio@gmail.com>
2019-12-04 13:34:13 -0300

    View structure simplification.

--- client/src/main/scala/crystal/View.scala
+++ client/src/main/scala/crystal/View.scala
@@ -9,10 +9,8 @@ import monocle.Lens
 import scala.language.higherKinds
 
 // ConcurrentEffect is needed by Flow, to .runCancelable the stream.
-sealed abstract class ViewBase[F[_] : ConcurrentEffect, A](stream: Stream[F, A]) {
+sealed class ViewRO[F[_] : ConcurrentEffect, A](val get: F[A], stream: Stream[F, A]) {
   lazy val flow: ReactFlowComponent[A] = Flow.flow(stream)
-
-  def get: F[A]
 
   // Useful for getting an algebra already in F[_].
   def algebra[H[_[_]]](implicit algebra: H[F]): H[F] = algebra
@@ -23,15 +21,12 @@ sealed abstract class ViewBase[F[_] : ConcurrentEffect, A](stream: Stream[F, A])
 }
 
 class View[F[_] : ConcurrentEffect, A](fixedLens: FixedLens[F, A], stream: Stream[F, A])
-  extends ViewBase[F, A](stream) with FixedLens[F, A] {
+  extends ViewRO[F, A](fixedLens.get, stream) with FixedLens[F, A] {
   // TODO Flow with pipe. But it can't be a def. We should use the same flow always in the same view.
   // Should it be a parameter of the View? A method .viewWithPipe that creates another flow?
   // Anyway, probably not all Views will have flows.... We can have a .withFlow([pipe]) maybe?
 
   // Convenience delegates for FixedLens
-  override def get: F[A] =
-    fixedLens.get
-
   override def set(value: A): F[Unit] =
     fixedLens.set(value)
 
@@ -45,7 +40,5 @@ class View[F[_] : ConcurrentEffect, A](fixedLens: FixedLens[F, A], stream: Strea
     new View(fixedLens compose otherLens, stream.map(otherLens.get))
   }
 }
-
-class ViewRO[F[_] : ConcurrentEffect, A](val get: F[A], stream: Stream[F, A]) extends ViewBase[F, A](stream)
 
 
